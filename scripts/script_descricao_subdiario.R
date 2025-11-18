4409b54458964ee9b348e5a244bbaadea949561a
Tomás Antonio <84979983+tqduval@users.noreply.github.com>
2025-11-18 10:02:28 -0300

    Atualização 'script_descricao_subdiario.qmd'

--- scripts/script_descricao_subdiario.R
+++ scripts/script_descricao_subdiario.R
@@ -41,35 +41,36 @@ min.years <- 1:10             # ao menos 1 ano até ao menos 10 anos de dados co
 
 # Criar tbl_df c/ possibilidades de falha e anos
 grid <-
-  expand_grid(threshold = thresholds, min_years = min.years) %>%       # combinações
+  expand_grid(threshold = thresholds, min_years = min.years) %>% # combinações
   mutate(n_gauges = purrr::map2_int(.x = threshold, .y = min_years, ~{ # analisar qtas estações p/ cada par
     df.quality %>% 
       filter(na_prct <= .x) %>% # filtrar estações c/ no máximo 'threshold' falhas
       count(gauge_code) %>%     # contar qtas estações
       filter(n >= .y) %>%       # filtrar estações c/ no mínimo 'min_years' anos
       nrow()                    # contar qtas estações
-  }))
+  })) # fim 'grid'
 
 # Visualização
-cores <- c("green4", "lightgreen", "orange", "#F10000")
-plot.anos.estacao <- 
+color <- c("green4", "lightgreen", "orange", "#F10000")
+plot.anos.estacao <- ({
   grid %>% 
-  mutate(threshold = factor(threshold, levels = thresholds)) %>% # ordenar valores de 'threshold'
-  ggplot(aes(x = min_years, y = n_gauges, fill = threshold)) +
-  geom_col() +
-  geom_text(aes(label = n_gauges), vjust = -0.3, size = 3, family = "serif") +
-  facet_wrap(~threshold, nrow = 4, labeller = labeller(threshold = function(x) paste0(x, "% falhas"))) +
-  scale_x_continuous(breaks = seq(min(grid$min_years), max(grid$min_years), by = 1)) +
-  scale_y_continuous(limits = c(0, 450)) +
-  scale_fill_manual(values = cores) +
-  labs(x = "Número de anos por estação",
-       y = "Número de estações") +
-  theme_minimal() +
-  theme(strip.text = element_text(face = "bold"),
-        legend.position =  "none",
-        plot.background = element_rect(color = "white"),
-        panel.border = element_rect(color = "black", fill = NA),
-        text = element_text(size = 10, family = "serif"))
+    mutate(threshold = factor(threshold, levels = thresholds)) %>% # ordenar valores de 'threshold'
+    ggplot(aes(x = min_years, y = n_gauges, fill = threshold)) +
+    geom_col() +
+    geom_text(aes(label = n_gauges), vjust = -0.3, size = 3, family = "serif") +
+    facet_wrap(~threshold, nrow = 4, labeller = labeller(threshold = function(x) paste0(x, "% falhas"))) +
+    scale_x_continuous(breaks = seq(min(grid$min_years), max(grid$min_years), by = 1)) +
+    scale_y_continuous(limits = c(0, 450)) +
+    scale_fill_manual(values = color) +
+    labs(x = "Número de anos por estação",
+         y = "Número de estações") +
+    theme_minimal() +
+    theme(strip.text = element_text(face = "bold"),
+          legend.position =  "none",
+          plot.background = element_rect(color = "white"),
+          panel.border = element_rect(color = "black", fill = NA),
+          text = element_text(size = 10, family = "serif"))
+}) # fim 'plot.anos.estacao'
 
 # ggsave(filename = "figuras/fig_anos_estacao.png", plot = plot.anos.estacao,
 #        width = 16, height = 14, units = "cm", dpi = 200)
@@ -189,13 +190,55 @@ plot_tau4_lm <- df_est %>%
         panel.border = element_rect(color = "black", fill = NA),
         text = element_text(family = "Times New Roman", color = "black", size = font_size)); plot_tau4_lm
 
-ggsave(plot = plot_tau4_lm, filename = "figuras/plot_tau4_lm.png",
-       width = 210, height = 297, dpi = 300, units = "mm",
-       # device = cairo_pdf,
-       ) # cairo_pdf plota textos c/ fontes que não são padrão
+# ggsave(plot = plot_tau4_lm, filename = "figuras/plot_tau4_lm.png",
+#        width = 210, height = 297, dpi = 300, units = "mm",
+#        # device = cairo_pdf,
+#        ) # cairo_pdf plota textos c/ fontes que não são padrão
 
 
 # RELAÇÕES ENTRE DURAÇÕES -------------------------------------------------
+
+# Calcular 1º e 2º momentos centrais p/ cada duração
+df.mom <- df_imax %>% 
+  group_by(d) %>% 
+  reframe(mean = mean(imax, na.rm = TRUE),
+          var = var(imax, na.rm = TRUE))
+
+# Visualização
+color <- c("mean" = "steelblue", "var" = "orange")
+plot.mom.scale <- ({
+  
+  df.mom %>%
+    pivot_longer(cols = c("mean", "var"),
+                 names_to = "moment",
+                 values_to = "value") %>% 
+    ggplot(aes(x = d, y = value)) +
+    facet_wrap(~moment, ncol = 2) +
+    geom_line() +
+    scale_x_log10() +
+    scale_y_log10() +
+    theme_minimal() +
+    theme(legend.position =  "none",
+          plot.background = element_rect(color = "white"),
+          panel.border = element_rect(color = "black", fill = NA),
+          text = element_text(family = "Times New Roman", color = "black", size = 10))
+  
+  df.mom %>%
+    pivot_longer(cols = c("mean", "var"),
+                 names_to = "moment",
+                 values_to = "value") %>% 
+    ggplot(aes(x = d, y = value, color = moment)) +
+    geom_line() +
+    scale_x_log10() +
+    scale_y_log10() +
+    scale_color_manual(values = color) +
+    theme_minimal() +
+    theme(legend.position =  "none",
+          plot.background = element_rect(color = "white"),
+          panel.border = element_rect(color = "black", fill = NA),
+          text = element_text(family = "Times New Roman", color = "black", size = 10))
+  
+})
 
 
 # SAZONALIDADE ------------------------------------------------------------
